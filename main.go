c0d1353a141dabefc6ae3f2966ce61670b17dc34
Wei Lok <wongweilok@disroot.org>
2021-01-11 15:11:03 +0800

    Remove duplicate intake codes

--- main.go
+++ main.go
@@ -20,16 +20,42 @@ type Timetable []struct {
 	EndTime string `json:"TIME_TO"`
 }
 
+func removeDup(intakes []string) []string {
+	intakeMap := make(map[string]bool)
+	intakeList := []string{}
+
+	for _, intake := range intakes {
+		if _, value := intakeMap[intake]; !value {
+			intakeMap[intake] = true
+			intakeList = append(intakeList, intake)
+		}
+	}
+
+	return intakeList
+}
+
 func main() {
+	// Create HTTPS Get request from open web service API
 	resp, _ := http.Get("https://s3-ap-southeast-1.amazonaws.com/open-ws/weektimetable")
 	bytes, _ := ioutil.ReadAll(resp.Body)
 	resp.Body.Close()
 
+	// Parse JSON into Timetable struct
 	var tb Timetable
 	err := json.Unmarshal([]byte(bytes), &tb)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println(tb)
+	// Add all intake codes into a slice
+	intakeListDup := []string{}
+
+	for i := range tb {
+		intakeListDup = append(intakeListDup, tb[i].Intake)
+	}
+
+	// Remove redundant intake codes
+	intakes := removeDup(intakeListDup)
+
+	fmt.Println(intakes)
 }
