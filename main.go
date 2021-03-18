0d29ac41c6b5dc670b06a2a6f0028eeec7452f99
Wong Wei Lok <wongweilok@disroot.org>
2021-03-18 21:15:43 +0800

    Change License

--- main.go
+++ main.go
@@ -1,4 +1,21 @@
-/* See LICENSE file for copyright and license details. */
+/*
+   Copyright (C) 2021 Wong Wei Lok <wongweilok@disroot.org>
+
+   This file is part of aputt.
+
+   aputt is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   aputt is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with aputt.  If not, see <https://www.gnu.org/licenses/>.
+*/
 
 package main
 
